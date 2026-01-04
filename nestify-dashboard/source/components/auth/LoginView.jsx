9026a70eb4626ff70ac165c1a298018c36b697bf
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-04 16:08:19 -0500

    Updated Login view.

--- nestify-dashboard/source/components/auth/LoginView.jsx
+++ nestify-dashboard/source/components/auth/LoginView.jsx
@@ -1,5 +1,36 @@
 const LoginView = function () {
-	return <h1>Login View</h1>;
+	return (
+		<div className="div-login-view-container">
+			<img
+				src="/media/images/logo-glt.png"
+				alt="Genesee Land Trust Logo"
+			/>
+			<div className="div-login-view-form-container">
+				<header className="header-login-view-form-container">
+					<h2>Conservation Dashboard</h2>
+					<p>Enter your credentials to access the dashboard.</p>
+				</header>
+				<form
+					className="form-login-view-container"
+					action="/backend/auth/route"
+					method="POST"
+				>
+					<div className="div-form-input-container">
+						<label htmlFor="username"></label>
+						<input id="username" type="text" name="username" />
+					</div>
+					<div className="div-form-input-container">
+						<label htmlFor="password"></label>
+						<input id="password" type="password" name="password" />
+					</div>
+					<button className="btn-form-submit" type="button">
+						<ion-icon src="/media/icons/icon-sign-in.svg"></ion-icon>
+						<span>Sign In</span>
+					</button>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default LoginView;
--- nestify-dashboard/source/login.jsx
+++ nestify-dashboard/source/login.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Login from "./components/auth/LoginView";
 
-const root = createRoot(document.querySelector(".main-container"));
+const root = createRoot(document.querySelector(".root"));
 
 root.render(
 	<StrictMode>
