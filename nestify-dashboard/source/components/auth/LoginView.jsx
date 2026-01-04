4ac82088508aac81e61922c330b233586e395a98
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-03 20:30:21 -0500

    Updated Login view.

---
+++ nestify-dashboard/source/components/auth/LoginView.jsx
@@ -0,0 +1,5 @@
+const LoginView = function () {
+	return <h1>Login View</h1>;
+};
+
+export default LoginView;
--- nestify-dashboard/source/login.jsx
+++ nestify-dashboard/source/login.jsx
@@ -1,6 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import Login from "./components/auth/LoginView";
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.querySelector(".main-container"));
 
-root.render(<StrictMode></StrictMode>);
+root.render(
+	<StrictMode>
+		<Login></Login>
+	</StrictMode>
+);
