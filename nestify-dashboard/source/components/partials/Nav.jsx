0c1f6b93547b53650673dd8cce3ec9d9becfe740
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-05 20:33:25 -0500

    Updated Nav partial files.

--- nestify-dashboard/source/components/partials/Nav.jsx
+++ nestify-dashboard/source/components/partials/Nav.jsx
@@ -6,24 +6,26 @@ import "../../assets/css/partials/nav.css";
 const Nav = function () {
 	return (
 		<div className="div-main-nav-container">
-			<Logo></Logo>
-			<nav className="main-nav-container">
-				<ul className="main-nav-container-list">
-					<li className="main-nav-container-list-item active">
-						<a href="#">Bird Boxes</a>
-					</li>
-					<li className="main-nav-container-list-item">
-						<a href="#">Report</a>
-					</li>
-					<li className="main-nav-container-list-item">
-						<a href="#">Map</a>
-					</li>
-				</ul>
-				<button>
-					<ion-icon src="/media/icons/icon-sign-out.svg"></ion-icon>
-					<span>Logout</span>
-				</button>
-			</nav>
+			<div className="div-main-edge-container">
+				<Logo></Logo>
+				<nav className="main-nav-container">
+					<ul className="main-nav-container-list">
+						<li className="main-nav-container-list-item active">
+							<a href="#">Bird Boxes</a>
+						</li>
+						<li className="main-nav-container-list-item">
+							<a href="#">Report</a>
+						</li>
+						<li className="main-nav-container-list-item">
+							<a href="#">Map</a>
+						</li>
+					</ul>
+					<button>
+						<ion-icon src="/media/icons/icon-sign-out.svg"></ion-icon>
+						<span>Logout</span>
+					</button>
+				</nav>
+			</div>
 		</div>
 	);
 };
