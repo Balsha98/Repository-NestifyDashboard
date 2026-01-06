db52560afe61d1b950c85f927443ebcc51ff7e31
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-06 17:02:41 -0500

    Minor code update.

--- nestify-dashboard/source/components/partials/Logo.jsx
+++ nestify-dashboard/source/components/partials/Logo.jsx
@@ -1,7 +1,5 @@
 const LogoView = function () {
-	return (
-		<img src="/media/images/logo-glt.png" alt="Genesee Land Trust Logo" />
-	);
+	return <img src="/media/images/logo-glt.png" alt="Genesee Land Trust Logo" />;
 };
 
 export default LogoView;
