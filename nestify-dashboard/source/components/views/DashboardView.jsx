6ba2755e7da7c23599a78a294775dbb730bb1f1c
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-06 13:11:15 -0500

    Updated Dashboard view.

--- nestify-dashboard/source/components/views/DashboardView.jsx
+++ nestify-dashboard/source/components/views/DashboardView.jsx
@@ -1,9 +1,52 @@
+import { useState } from "react";
 import Nav from "../partials/Nav";
 
 const DashboardView = function () {
+	const [selectBirdBox, setSelectBirdBox] = useState(null);
+
 	return (
 		<>
 			<Nav></Nav>
+			<div className="div-main-edge-container">
+				<div className="div-dashboard-view-container">
+					<header className="header-dashboard-view-container">
+						<div className="div-dashboard-view-birdbox-info-container">
+							<h2>BirdBox 1</h2>
+							<p>Cornwall Preserve</p>
+						</div>
+						<div className="div-dashboard-view-birdbox-editable-container">
+							<div className="div-birdbox-select-container">
+								<div className="div-selected-birdbox-info-container">
+									<div className="div-selected-birdbox-name-container">
+										<ion-icon src="/media/icons/icon-dot.svg"></ion-icon>
+										<p>Box 1 - Cornwall Preserve</p>
+									</div>
+									<ion-icon src="/media/icons/icon-chevron-down.svg"></ion-icon>
+								</div>
+								{selectBirdBox && (
+									<ul className="birdbox-options-list">
+										<li className="birdbox-options-list-item">
+											<span>Box 2 - Deer Creek Woods East</span>
+										</li>
+										<li className="birdbox-options-list-item">
+											<span>Box 3 - Deer Creek Woods West</span>
+										</li>
+										<li className="birdbox-options-list-item">
+											<span>Box 4 - Ganargua Creek Meadow Preserve</span>
+										</li>
+										<li className="birdbox-options-list-item">
+											<span>Box 5 - Irene Gossin Nature Preserve</span>
+										</li>
+									</ul>
+								)}
+							</div>
+							<button>
+								<ion-icon src="/media/icons/icon-plus.svg"></ion-icon>
+							</button>
+						</div>
+					</header>
+				</div>
+			</div>
 		</>
 	);
 };
