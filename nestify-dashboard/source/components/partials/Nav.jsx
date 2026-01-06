5395c59829f124a5bbe59efa1c931ff9ce12fa92
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-06 18:10:00 -0500

    Updated Nav partial files.

--- nestify-dashboard/source/components/partials/Nav.jsx
+++ nestify-dashboard/source/components/partials/Nav.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 // IMPORTED MODULES
 import Logo from "./Logo";
 import Logout from "./modals/Logout";
@@ -5,29 +6,38 @@ import Logout from "./modals/Logout";
 import "../../assets/css/partials/nav.css";
 
 const Nav = function () {
+	const [isLoggingOut, setIsLoggingOut] = useState(false);
+
+	const handleToggleLogoutModal = function () {
+		setIsLoggingOut((value) => !value);
+	};
+
 	return (
-		<div className="div-main-nav-container">
-			<div className="div-main-edge-container">
-				<Logo></Logo>
-				<nav className="main-nav-container">
-					<ul className="main-nav-container-list">
-						<li className="main-nav-container-list-item active">
-							<a href="#">Bird Boxes</a>
-						</li>
-						<li className="main-nav-container-list-item">
-							<a href="#">Report</a>
-						</li>
-						<li className="main-nav-container-list-item">
-							<a href="#">Map</a>
-						</li>
-					</ul>
-					<button>
-						<ion-icon src="/media/icons/icon-sign-out.svg"></ion-icon>
-						<span>Logout</span>
-					</button>
-				</nav>
+		<>
+			{isLoggingOut && <Logout onToggleLogoutModal={handleToggleLogoutModal}></Logout>}
+			<div className="div-main-nav-container">
+				<div className="div-main-edge-container">
+					<Logo></Logo>
+					<nav className="main-nav-container">
+						<ul className="main-nav-container-list">
+							<li className="main-nav-container-list-item active">
+								<a href="#">Bird Boxes</a>
+							</li>
+							<li className="main-nav-container-list-item">
+								<a href="#">Report</a>
+							</li>
+							<li className="main-nav-container-list-item">
+								<a href="#">Map</a>
+							</li>
+						</ul>
+						<button className={`${isLoggingOut ? "active" : ""}`} onClick={handleToggleLogoutModal}>
+							<ion-icon src="/media/icons/icon-sign-out.svg"></ion-icon>
+							<span>Logout</span>
+						</button>
+					</nav>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
