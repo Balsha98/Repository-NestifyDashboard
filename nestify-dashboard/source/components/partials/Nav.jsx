cc95883ff861270eac8fab413246e29faf26e63d
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-05 20:24:45 -0500

    Updated Nav partial files.

--- nestify-dashboard/source/components/partials/Nav.jsx
+++ nestify-dashboard/source/components/partials/Nav.jsx
@@ -1,1 +1,31 @@
+// IMPORTED MODULES
+import Logo from "./Logo";
+// IMPORTED STYLESHEETS
 import "../../assets/css/partials/nav.css";
+
+const Nav = function () {
+	return (
+		<div className="div-main-nav-container">
+			<Logo></Logo>
+			<nav className="main-nav-container">
+				<ul className="main-nav-container-list">
+					<li className="main-nav-container-list-item active">
+						<a href="#">Bird Boxes</a>
+					</li>
+					<li className="main-nav-container-list-item">
+						<a href="#">Report</a>
+					</li>
+					<li className="main-nav-container-list-item">
+						<a href="#">Map</a>
+					</li>
+				</ul>
+				<button>
+					<ion-icon src="/media/icons/icon-sign-out.svg"></ion-icon>
+					<span>Logout</span>
+				</button>
+			</nav>
+		</div>
+	);
+};
+
+export default Nav;
