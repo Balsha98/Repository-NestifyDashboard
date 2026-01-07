3a262d3e6555cb1e81d8a9f92132a1355e44d3f9
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-06 19:08:26 -0500

    Added BirdBox modal files.

---
+++ nestify-dashboard/source/components/partials/modals/BirdBox.jsx
@@ -0,0 +1,46 @@
+import "../../../assets/css/partials/modals/birdbox.css";
+
+const BirdBox = function ({ onToggleBirdBoxModal }) {
+	return (
+		// prettier-ignore
+		<div className="div-birdbox-modal-container">
+			<div className="div-birdbox-modal">
+				<header className="header-birdbox-modal">
+                    <div className="div-header-birdbox-modal-info-container">
+                        <ion-icon src="/media/icons/icon-plus.svg"></ion-icon>
+                        <h2>Add BirdBox</h2>
+                    </div>
+                    <button onClick={onToggleBirdBoxModal}>
+                        <ion-icon src="/media/icons/icon-close.svg"></ion-icon>
+                    </button>
+				</header>
+				<form className="form-birdbox-modal" action="" method="POST">
+                    <div className="div-form-input-container">
+                        <label htmlFor="box-name">Box Name *</label>
+                        <input id="box-name" type="text" name="box-name" placeholder="Box 1" />
+                    </div>
+                    <div className="div-form-input-container">
+                        <label htmlFor="location-name">Location Name *</label>
+                        <input id="location-name" type="text" name="location-name" placeholder="Location" />
+                    </div>
+                    <div className="div-form-multi-input-container">
+                        <div className="div-form-input-container">
+                            <label htmlFor="latitude">Latitude</label>
+                            <input id="latitude" type="text" name="latitude" placeholder="--" />
+                        </div>
+                        <div className="div-form-input-container">
+                            <label htmlFor="longitude">Longitude</label>
+                            <input id="longitude" type="text" name="longitude" placeholder="--" />
+                        </div>
+                    </div>
+                    <button className="btn-form-submit" type="button">
+                        <ion-icon src="/media/icons/icon-selected.svg"></ion-icon>
+                        <span>Add BirdBox</span>
+                    </button>
+                </form>
+			</div>
+		</div>
+	);
+};
+
+export default BirdBox;
