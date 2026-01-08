1fa6e17d1b1e3166d95bc7aac29ba1bfee9b2913
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-07 21:05:59 -0500

    Added Upload sign partial files.

---
+++ nestify-dashboard/source/components/partials/signs/Upload.jsx
@@ -0,0 +1,16 @@
+// IMPORTED STYLESHEETS
+import "../../../assets/css/partials/signs/upload.css";
+
+const Upload = function () {
+	return (
+		<div className="div-upload-sign-container">
+			<p>No data to display. Upload data to view information.</p>
+			<button>
+				<ion-icon src="/media/icons/icon-upload.svg"></ion-icon>
+				<span>Upload Data</span>
+			</button>
+		</div>
+	);
+};
+
+export default Upload;
