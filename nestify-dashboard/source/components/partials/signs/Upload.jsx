2a3af0dbcfe42856f38516a9c29ce7ca85312ee7
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-07 21:19:16 -0500

    Updated Upload sign.

--- nestify-dashboard/source/components/partials/signs/Upload.jsx
+++ nestify-dashboard/source/components/partials/signs/Upload.jsx
@@ -1,11 +1,11 @@
 // IMPORTED STYLESHEETS
 import "../../../assets/css/partials/signs/upload.css";
 
-const Upload = function () {
+const Upload = function ({ isUploadingData, onToggleUploadModal }) {
 	return (
 		<div className="div-upload-sign-container">
 			<p>No data to display. Upload data to view information.</p>
-			<button>
+			<button className={`${isUploadingData ? "active" : ""}`} onClick={onToggleUploadModal}>
 				<ion-icon src="/media/icons/icon-upload.svg"></ion-icon>
 				<span>Upload Data</span>
 			</button>
