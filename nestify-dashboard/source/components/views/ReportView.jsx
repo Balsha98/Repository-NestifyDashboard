9d0b41a1f01b39aaf8af9d665b2eceb7b2c81d06
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-11 14:20:10 -0500

    Added Report view files.

---
+++ nestify-dashboard/source/components/views/ReportView.jsx
@@ -0,0 +1,14 @@
+// IMPORTED MODULES
+import Nav from "../partials/Nav";
+// IMPORTED STYLESHEETS
+import "../../assets/css/views/report.css";
+
+const ReportView = function () {
+	return (
+		<>
+			<Nav></Nav>
+		</>
+	);
+};
+
+export default ReportView;
