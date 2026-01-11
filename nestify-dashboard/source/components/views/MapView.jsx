90f1b193adcdb07020790b2d690311460e324554
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-11 17:40:53 -0500

    Updated Map view.

--- nestify-dashboard/source/components/views/MapView.jsx
+++ nestify-dashboard/source/components/views/MapView.jsx
@@ -1,18 +1,23 @@
 import { useState } from "react";
 // IMPORTED MODULES
 import Nav from "../partials/Nav";
+import DetailsModal from "../partials/modals/Details";
 import BirdBoxModal from "../partials/modals/BirdBox";
 import LeafletMap from "../partials/views/map/Leaflet";
 import BirdBoxesList from "../partials/views/map/BirdBoxes";
 
 const MapView = function () {
+	const [isViewingDetails, setIsViewingDetails] = useState(true);
 	const [isAddingBox, setIsAddingBox] = useState(false);
+
+	const handleToggleDetailsModal = () => setIsViewingDetails((value) => !value);
 
 	const handleToggleBirdBoxModal = () => setIsAddingBox((value) => !value);
 
 	return (
 		<>
 			<Nav></Nav>
+			{isViewingDetails && <DetailsModal onToggleDetailsModal={handleToggleDetailsModal}></DetailsModal>}
 			{isAddingBox && <BirdBoxModal onToggleBirdBoxModal={handleToggleBirdBoxModal}></BirdBoxModal>}
 			<div className="div-main-edge-container">
 				<div className="div-map-view-container">
