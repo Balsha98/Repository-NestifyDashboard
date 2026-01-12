6567cc5bb27ed0a8cc16df8866b81d0acf2b2b61
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-12 16:55:39 -0500

    Updated Dashboard view.

--- nestify-dashboard/source/components/partials/views/dashboard/Container.jsx
+++ nestify-dashboard/source/components/partials/views/dashboard/Container.jsx
@@ -1,48 +1,43 @@
-import { useState } from "react";
 // IMPORTED STYLESHEETS
 import "../../../../assets/css/partials/views/dashboard/container.css";
 
-const Container = function ({ isUploadingData, onToggleUploadModal }) {
-	const [selectedInnerView, setSelectedInnerView] = useState("overview");
+const Container = function ({ isUploadingData, onToggleUploadModal, selectedInnerView, onToggleInnerView }) {
+    const innerViewNames = ["Overview", "Analytics", "Gallery", "Maintenance", "Settings"];
 
-	const innerViewNames = ["Overview", "Analytics", "Gallery", "Maintenance", "Settings"];
+    return (
+        <div className="div-dashboard-view-general-container">
+            <header className="header-dashboard-view-general-container">
+                <ul className="dashboard-view-general-toggle-list">
+                    {innerViewNames.map((innerViewName, i) => {
+                        const lowerCasedName = innerViewName.toLowerCase();
 
-	const handleToggleInnerView = function (e) {
-		setSelectedInnerView(e.target.closest("li").dataset.innerViewName);
-	};
-
-	return (
-		<div className="div-dashboard-view-general-container">
-			<header className="header-dashboard-view-general-container">
-				<ul className="dashboard-view-general-toggle-list">
-					{innerViewNames.map((innerViewName, i) => {
-						const lowerCasedName = innerViewName.toLowerCase();
-
-						return (
-							<li
-								key={i}
-								className={`dashboard-view-general-toggle-list-item ${selectedInnerView === lowerCasedName ? "active" : ""}`}
-								onClick={handleToggleInnerView}
-								data-inner-view-name={lowerCasedName}
-							>
-								<span>{innerViewName}</span>
-							</li>
-						);
-					})}
-				</ul>
-				<div className="div-dashboard-view-general-actions-container">
-					<button>
-						<ion-icon src="/media/icons/icon-download.svg"></ion-icon>
-						<span>Export Data</span>
-					</button>
-					<button className={`${isUploadingData ? "active" : ""}`} onClick={onToggleUploadModal}>
-						<ion-icon src="/media/icons/icon-upload.svg"></ion-icon>
-						<span>Upload Data</span>
-					</button>
-				</div>
-			</header>
-		</div>
-	);
+                        return (
+                            <li
+                                key={i}
+                                className={`dashboard-view-general-toggle-list-item ${
+                                    selectedInnerView === lowerCasedName ? "active" : ""
+                                }`}
+                                onClick={onToggleInnerView}
+                                data-inner-view-name={lowerCasedName}
+                            >
+                                <span>{innerViewName}</span>
+                            </li>
+                        );
+                    })}
+                </ul>
+                <div className="div-dashboard-view-general-actions-container">
+                    <button>
+                        <ion-icon src="/media/icons/icon-download.svg"></ion-icon>
+                        <span>Export Data</span>
+                    </button>
+                    <button className={`${isUploadingData ? "active" : ""}`} onClick={onToggleUploadModal}>
+                        <ion-icon src="/media/icons/icon-upload.svg"></ion-icon>
+                        <span>Upload Data</span>
+                    </button>
+                </div>
+            </header>
+        </div>
+    );
 };
 
 export default Container;
--- nestify-dashboard/source/components/views/DashboardView.jsx
+++ nestify-dashboard/source/components/views/DashboardView.jsx
@@ -5,76 +5,92 @@ import BirdBoxModal from "../partials/modals/BirdBox";
 import UploadSign from "../partials/signs/Upload";
 import UploadModal from "../partials/modals/Upload";
 import Container from "../partials/views/dashboard/Container";
+import Settings from "../partials/views/dashboard/Settings";
 
 const DashboardView = function () {
-	const [isAddingBox, setIsAddingBox] = useState(false);
-	const [selectBirdBox, setSelectBirdBox] = useState(false);
-	const [birdBoxData, setBirdBoxData] = useState(() => true);
-	const [isUploadingData, setIsUploadingData] = useState(false);
+    const [isAddingBox, setIsAddingBox] = useState(false);
+    const [selectBirdBox, setSelectBirdBox] = useState(false);
+    const [birdBoxData, setBirdBoxData] = useState(() => true);
+    const [isUploadingData, setIsUploadingData] = useState(false);
+    const [selectedInnerView, setSelectedInnerView] = useState("settings");
 
-	const handleSelectBirdBox = () => setSelectBirdBox((value) => !value);
+    const handleSelectBirdBox = () => setSelectBirdBox((value) => !value);
 
-	const handleToggleBirdBoxModal = () => setIsAddingBox((value) => !value);
+    const handleToggleBirdBoxModal = () => setIsAddingBox((value) => !value);
 
-	const handleToggleUploadModal = () => setIsUploadingData((value) => !value);
+    const handleToggleUploadModal = () => setIsUploadingData((value) => !value);
 
-	return (
-		<>
-			<Nav></Nav>
-			{isAddingBox && <BirdBoxModal onToggleBirdBoxModal={handleToggleBirdBoxModal}></BirdBoxModal>}
-			{isUploadingData && <UploadModal onToggleUploadModal={handleToggleUploadModal}></UploadModal>}
-			<div className="div-main-edge-container">
-				<div className="div-dashboard-view-container">
-					<header className="header-dashboard-view-container">
-						<div className="div-header-dashboard-view-birdbox-info-container">
-							<h2>BirdBox 1</h2>
-							<p>Cornwall Preserve</p>
-						</div>
-						<div className="div-dashboard-view-birdbox-editable-container">
-							<div className="div-birdbox-select-container">
-								<div className="div-selected-birdbox-info-container" onClick={handleSelectBirdBox}>
-									<div className="div-selected-birdbox-name-container">
-										<ion-icon src="/media/icons/icon-dot.svg"></ion-icon>
-										<p>Box 1 - Cornwall Preserve</p>
-									</div>
-									<ion-icon src="/media/icons/icon-chevron-down.svg"></ion-icon>
-								</div>
-								{selectBirdBox && (
-									<ul className="birdbox-options-list">
-										<li className="birdbox-options-list-item active">
-											<span>Box 1 - Cornwall Preserve</span>
-											<ion-icon src="/media/icons/icon-selected.svg"></ion-icon>
-										</li>
-										<li className="birdbox-options-list-item">
-											<span>Box 2 - Deer Creek Woods East</span>
-											<ion-icon src="/media/icons/icon-selected.svg"></ion-icon>
-										</li>
-										<li className="birdbox-options-list-item">
-											<span>Box 3 - Deer Creek Woods West</span>
-											<ion-icon src="/media/icons/icon-selected.svg"></ion-icon>
-										</li>
-										<li className="birdbox-options-list-item">
-											<span>Box 4 - Ganargua Creek Meadow Preserve</span>
-											<ion-icon src="/media/icons/icon-selected.svg"></ion-icon>
-										</li>
-										<li className="birdbox-options-list-item">
-											<span>Box 5 - Irene Gossin Nature Preserve</span>
-											<ion-icon src="/media/icons/icon-selected.svg"></ion-icon>
-										</li>
-									</ul>
-								)}
-							</div>
-							<button className={`${isAddingBox ? "active" : ""}`} onClick={handleToggleBirdBoxModal}>
-								<ion-icon src="/media/icons/icon-plus.svg"></ion-icon>
-							</button>
-						</div>
-					</header>
-					{birdBoxData || <UploadSign isUploadingData={isUploadingData} onToggleUploadModal={handleToggleUploadModal}></UploadSign>}
-					{birdBoxData && <Container isUploadingData={isUploadingData} onToggleUploadModal={handleToggleUploadModal}></Container>}
-				</div>
-			</div>
-		</>
-	);
+    const handleToggleInnerView = (e) => setSelectedInnerView(e.target.closest("li").dataset.innerViewName);
+
+    return (
+        <>
+            <Nav></Nav>
+            {isAddingBox && <BirdBoxModal onToggleBirdBoxModal={handleToggleBirdBoxModal}></BirdBoxModal>}
+            {isUploadingData && <UploadModal onToggleUploadModal={handleToggleUploadModal}></UploadModal>}
+            <div className="div-main-edge-container">
+                <div className="div-dashboard-view-container">
+                    <header className="header-dashboard-view-container">
+                        <div className="div-header-dashboard-view-birdbox-info-container">
+                            <h2>BirdBox 1</h2>
+                            <p>Cornwall Preserve</p>
+                        </div>
+                        <div className="div-dashboard-view-birdbox-editable-container">
+                            <div className="div-birdbox-select-container">
+                                <div className="div-selected-birdbox-info-container" onClick={handleSelectBirdBox}>
+                                    <div className="div-selected-birdbox-name-container">
+                                        <ion-icon src="/media/icons/icon-dot.svg"></ion-icon>
+                                        <p>Box 1 - Cornwall Preserve</p>
+                                    </div>
+                                    <ion-icon src="/media/icons/icon-chevron-down.svg"></ion-icon>
+                                </div>
+                                {selectBirdBox && (
+                                    <ul className="birdbox-options-list">
+                                        <li className="birdbox-options-list-item active">
+                                            <span>Box 1 - Cornwall Preserve</span>
+                                            <ion-icon src="/media/icons/icon-selected.svg"></ion-icon>
+                                        </li>
+                                        <li className="birdbox-options-list-item">
+                                            <span>Box 2 - Deer Creek Woods East</span>
+                                            <ion-icon src="/media/icons/icon-selected.svg"></ion-icon>
+                                        </li>
+                                        <li className="birdbox-options-list-item">
+                                            <span>Box 3 - Deer Creek Woods West</span>
+                                            <ion-icon src="/media/icons/icon-selected.svg"></ion-icon>
+                                        </li>
+                                        <li className="birdbox-options-list-item">
+                                            <span>Box 4 - Ganargua Creek Meadow Preserve</span>
+                                            <ion-icon src="/media/icons/icon-selected.svg"></ion-icon>
+                                        </li>
+                                        <li className="birdbox-options-list-item">
+                                            <span>Box 5 - Irene Gossin Nature Preserve</span>
+                                            <ion-icon src="/media/icons/icon-selected.svg"></ion-icon>
+                                        </li>
+                                    </ul>
+                                )}
+                            </div>
+                            <button className={`${isAddingBox ? "active" : ""}`} onClick={handleToggleBirdBoxModal}>
+                                <ion-icon src="/media/icons/icon-plus.svg"></ion-icon>
+                            </button>
+                        </div>
+                    </header>
+                    {birdBoxData || (
+                        <UploadSign isUploadingData={isUploadingData} onToggleUploadModal={handleToggleUploadModal} />
+                    )}
+                    {birdBoxData && (
+                        <>
+                            <Container
+                                isUploadingData={isUploadingData}
+                                onToggleUploadModal={handleToggleUploadModal}
+                                selectedInnerView={selectedInnerView}
+                                onToggleInnerView={handleToggleInnerView}
+                            />
+                            {selectedInnerView === "settings" && <Settings />}
+                        </>
+                    )}
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default DashboardView;
