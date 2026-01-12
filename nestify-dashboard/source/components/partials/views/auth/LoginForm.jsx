6052e84071a62a660f3d7fa36101938d4ddf2c75
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-12 16:55:17 -0500

    Minor style updates.

--- nestify-dashboard/source/components/partials/views/auth/LoginForm.jsx
+++ nestify-dashboard/source/components/partials/views/auth/LoginForm.jsx
@@ -1,30 +1,30 @@
 const LogoFormView = function () {
-	return (
-		<div className="div-login-view-form-container">
-			<header className="header-login-view-form-container">
-				<h2>Conservation Dashboard</h2>
-				<p>Enter your credentials to access the dashboard.</p>
-			</header>
-			<form className="form-login-view-container" action="/backend/auth/route" method="POST">
-				<div className="div-form-login-input-container">
-					<label htmlFor="username">
-						<ion-icon src="/media/icons/icon-user.svg"></ion-icon>
-					</label>
-					<input id="username" type="text" name="username" autoComplete="true" placeholder="Enter your username..." />
-				</div>
-				<div className="div-form-login-input-container">
-					<label htmlFor="password">
-						<ion-icon src="/media/icons/icon-password.svg"></ion-icon>
-					</label>
-					<input id="password" type="password" name="password" placeholder="Enter your password..." />
-				</div>
-				<button type="button">
-					<ion-icon src="/media/icons/icon-sign-in.svg"></ion-icon>
-					<span>Sign In</span>
-				</button>
-			</form>
-		</div>
-	);
+    return (
+        <div className="div-login-view-form-container">
+            <header className="header-login-view-form-container">
+                <h2>Conservation Dashboard</h2>
+                <p>Enter your credentials to access the dashboard.</p>
+            </header>
+            <form className="form-login-view-container" action="" method="POST">
+                <div className="div-form-login-input-container">
+                    <label htmlFor="username">
+                        <ion-icon src="/media/icons/icon-user.svg"></ion-icon>
+                    </label>
+                    <input id="username" type="text" name="username" autoComplete="true" placeholder="Enter your username..." />
+                </div>
+                <div className="div-form-login-input-container">
+                    <label htmlFor="password">
+                        <ion-icon src="/media/icons/icon-password.svg"></ion-icon>
+                    </label>
+                    <input id="password" type="password" name="password" placeholder="Enter your password..." />
+                </div>
+                <button type="button">
+                    <ion-icon src="/media/icons/icon-sign-in.svg"></ion-icon>
+                    <span>Sign In</span>
+                </button>
+            </form>
+        </div>
+    );
 };
 
 export default LogoFormView;
