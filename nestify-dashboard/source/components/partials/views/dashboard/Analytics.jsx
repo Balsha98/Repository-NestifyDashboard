214d9c8c68fd37977bf677a13bf07eb76be46f79
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-15 16:09:45 -0500

    Added Analytics partial view files.

---
+++ nestify-dashboard/source/components/partials/views/dashboard/Analytics.jsx
@@ -0,0 +1,8 @@
+// IMPORTED STYLESHEETS
+import "../../../../assets/css/partials/views/dashboard/analytics.css";
+
+const Analytics = function () {
+    return <div className="div-dashboard-view-analytics-container"></div>;
+};
+
+export default Analytics;
