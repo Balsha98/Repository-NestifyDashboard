ebed55eeb3758edf771deab2d8dc66ea8da2518a
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-17 14:16:57 -0500

    Updated Maintenance modal view.

--- nestify-dashboard/source/components/partials/views/dashboard/Maintenance.jsx
+++ nestify-dashboard/source/components/partials/views/dashboard/Maintenance.jsx
@@ -1,46 +1,56 @@
+import { useState } from "react";
 // IMPORTED STYLESHEETS
 import "../../../../assets/css/partials/views/dashboard/maintenance.css";
+// IMPORTED MODULES
+import ScheduleModal from "../../modals/Schedule";
 
 const Maintenance = function () {
+    const [isViewingSchedule, setIsViewingSchedule] = useState(false);
+
+    const handleToggleScheduleModal = () => setIsViewingSchedule((value) => !value);
+
     return (
-        <div className="div-dashboard-view-maintenance-container">
-            <div className="div-dashboard-view-maintenance-log-container">
-                <h2>Maintenance Log</h2>
-                <ul className="dashboard-view-maintenance-log-list">
-                    <li className="dashboard-view-maintenance-log-list-item">
-                        <header className="header-dashboard-view-maintenance-log-list-item">
+        <>
+            {isViewingSchedule && <ScheduleModal onToggleScheduleModal={handleToggleScheduleModal} />}
+            <div className="div-dashboard-view-maintenance-container">
+                <div className="div-dashboard-view-maintenance-log-container">
+                    <h2>Maintenance Log</h2>
+                    <ul className="dashboard-view-maintenance-log-list">
+                        <li className="dashboard-view-maintenance-log-list-item">
+                            <header className="header-dashboard-view-maintenance-log-list-item">
+                                <p>Battery Replacement</p>
+                                <span>01/13/2026</span>
+                            </header>
+                            <span>John Doe</span>
+                        </li>
+                        <li className="dashboard-view-maintenance-log-list-item">
+                            <header className="header-dashboard-view-maintenance-log-list-item">
+                                <p>Battery Replacement</p>
+                                <span>01/13/2026</span>
+                            </header>
+                            <span>John Doe</span>
+                        </li>
+                    </ul>
+                    <button>
+                        <ion-icon src="/media/icons/icon-file.svg"></ion-icon>
+                        <span>View Full Log</span>
+                    </button>
+                </div>
+                <div className="div-dashboard-view-maintenance-schedule-container">
+                    <h2>Maintenance Schedule</h2>
+                    <ul className="dashboard-view-maintenance-schedule-list">
+                        <li className="dashboard-view-maintenance-schedule-list-item">
                             <p>Battery Replacement</p>
                             <span>01/13/2026</span>
-                        </header>
-                        <span>John Doe</span>
-                    </li>
-                    <li className="dashboard-view-maintenance-log-list-item">
-                        <header className="header-dashboard-view-maintenance-log-list-item">
-                            <p>Battery Replacement</p>
-                            <span>01/13/2026</span>
-                        </header>
-                        <span>John Doe</span>
-                    </li>
-                </ul>
-                <button>
-                    <ion-icon src="/media/icons/icon-file.svg"></ion-icon>
-                    <span>View Full Log</span>
-                </button>
+                        </li>
+                    </ul>
+                    <button onClick={handleToggleScheduleModal}>
+                        <ion-icon src="/media/icons/icon-settings.svg"></ion-icon>
+                        <span>Schedule Maintenance</span>
+                    </button>
+                </div>
             </div>
-            <div className="div-dashboard-view-maintenance-schedule-container">
-                <h2>Maintenance Schedule</h2>
-                <ul className="dashboard-view-maintenance-schedule-list">
-                    <li className="dashboard-view-maintenance-schedule-list-item">
-                        <p>Battery Replacement</p>
-                        <span>01/13/2026</span>
-                    </li>
-                </ul>
-                <button>
-                    <ion-icon src="/media/icons/icon-settings.svg"></ion-icon>
-                    <span>Schedule Maintenance</span>
-                </button>
-            </div>
-        </div>
+        </>
     );
 };
 
