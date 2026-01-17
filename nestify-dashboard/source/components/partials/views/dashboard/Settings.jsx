c4bdf54c4bfba84eea4d3910c1dd320b500a88d1
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-17 13:32:27 -0500

    Updated Settings partial view.

--- nestify-dashboard/source/components/partials/views/dashboard/Settings.jsx
+++ nestify-dashboard/source/components/partials/views/dashboard/Settings.jsx
@@ -34,26 +34,26 @@ const Settings = function () {
             </header>
             <form className="form-dashboard-view-settings-container" action="" method="PUT">
                 <div className="div-form-dashboard-view-settings-multi-input-container">
-                    <div className="div-form-dashboard-view-settings-input-container">
+                    <div className={`div-form-dashboard-view-settings-input-container ${isDisabled ? "disabled" : ""}`}>
                         <label htmlFor="box-name">Box Name</label>
                         <input id="box-name" type="text" name="box-name" placeholder="BirdBox 1" disabled={isDisabled} />
                     </div>
-                    <div className="div-form-dashboard-view-settings-input-container">
+                    <div className={`div-form-dashboard-view-settings-input-container ${isDisabled ? "disabled" : ""}`}>
                         <label htmlFor="location-name">Location Name</label>
                         <input id="location-name" type="text" name="location-name" placeholder="Cornwall Preserve" disabled={isDisabled} />
                     </div>
                 </div>
                 <div className="div-form-dashboard-view-settings-multi-input-container">
-                    <div className="div-form-dashboard-view-settings-input-container">
+                    <div className={`div-form-dashboard-view-settings-input-container ${isDisabled ? "disabled" : ""}`}>
                         <label htmlFor="latitude">Latitude</label>
                         <input id="latitude" type="text" name="latitude" placeholder="--" disabled={isDisabled} />
                     </div>
-                    <div className="div-form-dashboard-view-settings-input-container">
+                    <div className={`div-form-dashboard-view-settings-input-container ${isDisabled ? "disabled" : ""}`}>
                         <label htmlFor="longitude">Longitude</label>
                         <input id="longitude" type="text" name="longitude" placeholder="--" disabled={isDisabled} />
                     </div>
                 </div>
-                <div className="div-form-dashboard-view-settings-input-container">
+                <div className={`div-form-dashboard-view-settings-input-container ${isDisabled ? "disabled" : ""}`}>
                     <label htmlFor="notes">Notes</label>
                     <textarea id="notes" name="notes" rows="8" placeholder="Additional notes about this box..." disabled={isDisabled}></textarea>
                 </div>
