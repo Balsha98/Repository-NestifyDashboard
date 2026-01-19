171937af635f6a448139d3fd013db548b453a618
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-19 12:26:47 -0500

    Added Invalid view partial files.

---
+++ nestify-dashboard/source/assets/components/partials/views/invalid/Info.jsx
@@ -0,0 +1,19 @@
+// IMPORTED STYLESHEETS
+import "../../../../css/partials/views/invalid/info.css";
+
+const Info = function () {
+    return (
+        <div className="div-invalid-view-modal-info-container">
+            <header className="header-invalid-view-modal-info-container">
+                <h2>Requested Page Does Not Exist</h2>
+                <p>The page you requested unfortunately does not exist.</p>
+            </header>
+            <a href="/dashboard">
+                <ion-icon src="/media/icons/icon-sign-in.svg"></ion-icon>
+                <span>Back to Dashboard</span>
+            </a>
+        </div>
+    );
+};
+
+export default Info;
