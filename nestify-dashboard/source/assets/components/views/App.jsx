b3dec3916b35a82c123007869bee2b5dec64413f
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-19 11:59:27 -0500

    Updated App routing.

--- nestify-dashboard/source/assets/components/views/App.jsx
+++ nestify-dashboard/source/assets/components/views/App.jsx
@@ -4,15 +4,17 @@ import LoginView from "./Login";
 import DashboardView from "./Dashboard";
 import ReportView from "./Report";
 import MapView from "./Map";
+import InvalidView from "./Invalid";
 
 const App = function () {
     return (
         <Routes>
-            <Route path="/" element={<LoginView />}></Route>
-            <Route path="/login" element={<LoginView />}></Route>
-            <Route path="/dashboard" element={<DashboardView />}></Route>
-            <Route path="/report" element={<ReportView />}></Route>
-            <Route path="/map" element={<MapView />}></Route>
+            <Route path="/" element={<LoginView />} />
+            <Route path="/login" element={<LoginView />} />
+            <Route path="/dashboard" element={<DashboardView />} />
+            <Route path="/report" element={<ReportView />} />
+            <Route path="/map" element={<MapView />} />
+            <Route path="*" element={<InvalidView />} />
         </Routes>
     );
 };
