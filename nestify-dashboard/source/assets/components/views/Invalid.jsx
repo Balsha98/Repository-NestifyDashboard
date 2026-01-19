85bcbeda96e55af7b0982a47253efea5bbcec0ba
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-19 12:26:23 -0500

    Added Invalid view.

---
+++ nestify-dashboard/source/assets/components/views/Invalid.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+// IMPORTED STYLESHEETS
+import "../../css/views/invalid.css";
+// IMPORTED MODULES
+import Logo from "../partials/Logo";
+import Info from "../partials/views/invalid/Info";
+
+const Invalid = function () {
+    useEffect(function () {
+        document.title = "Nestify | Invalid Route";
+    }, []);
+
+    return (
+        <div className="div-invalid-view-container">
+            <div className="div-invalid-view-modal">
+                <Logo />
+                <Info />
+            </div>
+        </div>
+    );
+};
+
+export default Invalid;
