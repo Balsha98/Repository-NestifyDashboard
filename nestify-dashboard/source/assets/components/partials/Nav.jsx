8dbd876e01aa3d3f2b672828802c3d74af1296c5
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-27 23:13:56 -0500

    Updated Nav partial view.

--- nestify-dashboard/source/assets/components/partials/Nav.jsx
+++ nestify-dashboard/source/assets/components/partials/Nav.jsx
@@ -5,49 +5,53 @@ import Logo from "./Logo";
 import Logout from "./modals/Logout";
 // IMPORTED STYLESHEETS
 import "../../css/partials/nav.css";
+import "../../css/responsive/partials/nav.css";
 
 const Nav = function () {
-    const [isLoggingOut, setIsLoggingOut] = useState(false);
+	const [isLoggingOut, setIsLoggingOut] = useState(false);
 
-    const handleToggleLogoutModal = () => setIsLoggingOut((value) => !value);
+	const handleToggleLogoutModal = () => setIsLoggingOut((value) => !value);
 
-    return (
-        <>
-            <Activity mode={isLoggingOut ? "visible" : "hidden"}>
-                <Logout onToggleLogoutModal={handleToggleLogoutModal} />
-            </Activity>
-            <div className="div-main-nav-container">
-                <div className="div-main-edge-container">
-                    <a href="/dashboard">
-                        <Logo />
-                    </a>
-                    <nav className="main-nav-container">
-                        <ul className="main-nav-container-list">
-                            <li className="main-nav-container-list-item">
-                                <NavLink to="/dashboard" className={({ isActive }) => (isActive ? "active" : "")}>
-                                    <span>Dashboard</span>
-                                </NavLink>
-                            </li>
-                            <li className="main-nav-container-list-item">
-                                <NavLink to="/report" className={({ isActive }) => (isActive ? "active" : "")}>
-                                    <span>Report</span>
-                                </NavLink>
-                            </li>
-                            <li className="main-nav-container-list-item">
-                                <NavLink to="/map" className={({ isActive }) => (isActive ? "active" : "")}>
-                                    <span>Map</span>
-                                </NavLink>
-                            </li>
-                        </ul>
-                        <button className={`${isLoggingOut ? "active" : ""}`} onClick={handleToggleLogoutModal}>
-                            <ion-icon src="/media/icons/icon-sign-out.svg" />
-                            <span>Logout</span>
-                        </button>
-                    </nav>
-                </div>
-            </div>
-        </>
-    );
+	return (
+		<>
+			<Activity mode={isLoggingOut ? "visible" : "hidden"}>
+				<Logout onToggleLogoutModal={handleToggleLogoutModal} />
+			</Activity>
+			<div className="div-main-nav-container">
+				<div className="div-main-edge-container">
+					<a href="/dashboard">
+						<Logo />
+					</a>
+					<nav className="main-nav-container">
+						<ul className="main-nav-container-list">
+							<li className="main-nav-container-list-item">
+								<NavLink to="/dashboard" className={({ isActive }) => (isActive ? "active" : "")}>
+									<span>Dashboard</span>
+								</NavLink>
+							</li>
+							<li className="main-nav-container-list-item">
+								<NavLink to="/report" className={({ isActive }) => (isActive ? "active" : "")}>
+									<span>Report</span>
+								</NavLink>
+							</li>
+							<li className="main-nav-container-list-item">
+								<NavLink to="/map" className={({ isActive }) => (isActive ? "active" : "")}>
+									<span>Map</span>
+								</NavLink>
+							</li>
+						</ul>
+						<button className={`${isLoggingOut ? "active" : ""}`} onClick={handleToggleLogoutModal}>
+							<ion-icon src="/media/icons/icon-sign-out.svg" />
+							<span>Logout</span>
+						</button>
+					</nav>
+					<button>
+						<ion-icon src="/media/icons/icon-menu.svg"></ion-icon>
+					</button>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Nav;
