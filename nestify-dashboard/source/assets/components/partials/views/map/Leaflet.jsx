12e0f312821ba8c59406fc1840fd3af806e1583d
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-28 16:54:55 -0500

    Updated Map view mobile responsiveness.

--- nestify-dashboard/source/assets/components/partials/views/map/Leaflet.jsx
+++ nestify-dashboard/source/assets/components/partials/views/map/Leaflet.jsx
@@ -1,8 +1,15 @@
 // IMPORTED STYLESHEETS
 import "../../../../css/partials/views/map/leaflet.css";
+import "../../../../css/responsive/partials/views/map/leaflet.css";
 
-const Leaflet = function () {
-    return <div className="div-map-view-leaflet-container"></div>;
+const Leaflet = function ({ onToggleBirdBoxesList }) {
+    return (
+        <div className="div-map-view-leaflet-container">
+            <button onClick={onToggleBirdBoxesList}>
+                <ion-icon src="/media/icons/icon-bird-house.svg"></ion-icon>
+            </button>
+        </div>
+    );
 };
 
 export default Leaflet;
--- nestify-dashboard/source/assets/components/views/Map.jsx
+++ nestify-dashboard/source/assets/components/views/Map.jsx
@@ -1,6 +1,7 @@
 import { Activity, useEffect, useState } from "react";
 // IMPORTED STYLESHEETS
 import "../../css/views/map.css";
+import "../../css/responsive/views/map.css";
 // IMPORTED MODULES
 import Nav from "../partials/Nav";
 import DetailsModal from "../partials/modals/Details";
@@ -11,10 +12,13 @@ import BirdBoxesList from "../partials/views/map/BirdBoxes";
 const Map = function () {
     const [isViewingDetails, setIsViewingDetails] = useState(false);
     const [isAddingBox, setIsAddingBox] = useState(false);
+    const [isViewingBoxes, setIsViewingBoxes] = useState(false);
 
     const handleToggleDetailsModal = () => setIsViewingDetails((value) => !value);
 
     const handleToggleBirdBoxModal = () => setIsAddingBox((value) => !value);
+
+    const handleToggleBirdBoxesList = () => setIsViewingBoxes((value) => !value);
 
     useEffect(function () {
         document.title = "Nestify | Map";
@@ -31,8 +35,13 @@ const Map = function () {
             </Activity>
             <div className="div-main-edge-container">
                 <div className="div-map-view-container">
-                    <LeafletMap />
-                    <BirdBoxesList isAddingBox={isAddingBox} onToggleBirdBoxModal={handleToggleBirdBoxModal} />
+                    <LeafletMap onToggleBirdBoxesList={handleToggleBirdBoxesList} />
+                    <BirdBoxesList
+                        isAddingBox={isAddingBox}
+                        onToggleBirdBoxModal={handleToggleBirdBoxModal}
+                        isViewingBoxes={isViewingBoxes}
+                        onToggleBirdBoxesList={handleToggleBirdBoxesList}
+                    />
                 </div>
             </div>
         </>
