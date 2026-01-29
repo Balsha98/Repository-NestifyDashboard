a7ff43930dbd5ae902ccb04f14a22fd4e4e8a6f4
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-29 12:35:28 -0500

    Added Notification modal partial files.

---
+++ nestify-dashboard/source/assets/components/partials/modals/Notification.jsx
@@ -0,0 +1,13 @@
+// IMPORTED STYLESHEETS
+import "../../../css/partials/modals/notification.css";
+
+const Notification = function () {
+    return (
+        <div className="div-notification-modal-container">
+            <ion-icon src="/media/icons/icon-notification.svg"></ion-icon>
+            <p>Notification message goes here.</p>
+        </div>
+    );
+};
+
+export default Notification;
--- nestify-dashboard/source/assets/components/views/Dashboard.jsx
+++ nestify-dashboard/source/assets/components/views/Dashboard.jsx
@@ -13,111 +13,113 @@ import Analytics from "../partials/views/dashboard/Analytics";
 import Gallery from "../partials/views/dashboard/Gallery";
 import Maintenance from "../partials/views/dashboard/Maintenance";
 import Settings from "../partials/views/dashboard/Settings";
+import NotificationModal from "../partials/modals/Notification";
 
 const Dashboard = function () {
-	const [isAddingBox, setIsAddingBox] = useState(false);
-	const [selectBirdBox, setSelectBirdBox] = useState(false);
-	const [birdBoxData, setBirdBoxData] = useState(() => true);
-	const [isUploadingData, setIsUploadingData] = useState(false);
-	const [selectedInnerView, setSelectedInnerView] = useState("overview");
+    const [isAddingBox, setIsAddingBox] = useState(false);
+    const [selectBirdBox, setSelectBirdBox] = useState(false);
+    const [birdBoxData, setBirdBoxData] = useState(() => true);
+    const [isUploadingData, setIsUploadingData] = useState(false);
+    const [selectedInnerView, setSelectedInnerView] = useState("overview");
 
-	const handleSelectBirdBox = () => setSelectBirdBox((value) => !value);
+    const handleSelectBirdBox = () => setSelectBirdBox((value) => !value);
 
-	const handleToggleBirdBoxModal = () => setIsAddingBox((value) => !value);
+    const handleToggleBirdBoxModal = () => setIsAddingBox((value) => !value);
 
-	const handleToggleUploadModal = () => setIsUploadingData((value) => !value);
+    const handleToggleUploadModal = () => setIsUploadingData((value) => !value);
 
-	const handleToggleInnerView = (e) => setSelectedInnerView(e.target.closest("button").dataset.innerViewName);
+    const handleToggleInnerView = (e) => setSelectedInnerView(e.target.closest("button").dataset.innerViewName);
 
-	useEffect(function () {
-		document.title = "Nestify | Dashboard";
-	}, []);
+    useEffect(function () {
+        document.title = "Nestify | Dashboard";
+    }, []);
 
-	return (
-		<>
-			<Nav />
-			<Activity mode={isAddingBox ? "visible" : "hidden"}>
-				<BirdBoxModal onToggleBirdBoxModal={handleToggleBirdBoxModal} />
-			</Activity>
-			<Activity mode={isUploadingData ? "visible" : "hidden"}>
-				<UploadModal onToggleUploadModal={handleToggleUploadModal} />
-			</Activity>
-			<div className="div-main-edge-container">
-				<div className="div-dashboard-view-container">
-					<header className="header-dashboard-view-container">
-						<div className="div-header-dashboard-view-birdbox-info-container">
-							<h2>BirdBox 1</h2>
-							<p>Cornwall Preserve</p>
-						</div>
-						<div className="div-dashboard-view-birdbox-editable-container">
-							<div className="div-birdbox-select-container">
-								<div className="div-selected-birdbox-info-container" onClick={handleSelectBirdBox}>
-									<div className="div-selected-birdbox-name-container">
-										<ion-icon src="/media/icons/icon-dot.svg" />
-										<p>Box 1 - Cornwall Preserve</p>
-									</div>
-									<ion-icon src="/media/icons/icon-chevron-down.svg" />
-								</div>
-								{selectBirdBox && (
-									<ul className="birdbox-options-list">
-										<li className="birdbox-options-list-item active">
-											<span>Box 1 - Cornwall Preserve</span>
-											<ion-icon src="/media/icons/icon-selected.svg" />
-										</li>
-										<li className="birdbox-options-list-item">
-											<span>Box 2 - Deer Creek Woods East</span>
-											<ion-icon src="/media/icons/icon-selected.svg" />
-										</li>
-										<li className="birdbox-options-list-item">
-											<span>Box 3 - Deer Creek Woods West</span>
-											<ion-icon src="/media/icons/icon-selected.svg" />
-										</li>
-										<li className="birdbox-options-list-item">
-											<span>Box 4 - Ganargua Creek Meadow Preserve</span>
-											<ion-icon src="/media/icons/icon-selected.svg" />
-										</li>
-										<li className="birdbox-options-list-item">
-											<span>Box 5 - Irene Gossin Nature Preserve</span>
-											<ion-icon src="/media/icons/icon-selected.svg" />
-										</li>
-									</ul>
-								)}
-							</div>
-							<button className={`${isAddingBox ? "active" : ""}`} onClick={handleToggleBirdBoxModal}>
-								<ion-icon src="/media/icons/icon-plus.svg" />
-							</button>
-						</div>
-					</header>
-					<Activity mode={birdBoxData ? "hidden" : "visible"}>
-						<UploadSign isUploadingData={isUploadingData} onToggleUploadModal={handleToggleUploadModal} />
-					</Activity>
-					<Activity mode={birdBoxData ? "visible" : "hidden"}>
-						<General
-							isUploadingData={isUploadingData}
-							onToggleUploadModal={handleToggleUploadModal}
-							selectedInnerView={selectedInnerView}
-							onToggleInnerView={handleToggleInnerView}
-						/>
-						<Activity mode={selectedInnerView === "overview" ? "visible" : "hidden"}>
-							<Overview onToggleInnerView={handleToggleInnerView} />
-						</Activity>
-						<Activity mode={selectedInnerView === "analytics" ? "visible" : "hidden"}>
-							<Analytics />
-						</Activity>
-						<Activity mode={selectedInnerView === "gallery" ? "visible" : "hidden"}>
-							<Gallery />
-						</Activity>
-						<Activity mode={selectedInnerView === "maintenance" ? "visible" : "hidden"}>
-							<Maintenance />
-						</Activity>
-						<Activity mode={selectedInnerView === "settings" ? "visible" : "hidden"}>
-							<Settings />
-						</Activity>
-					</Activity>
-				</div>
-			</div>
-		</>
-	);
+    return (
+        <>
+            <Nav />
+            <Activity mode={isAddingBox ? "visible" : "hidden"}>
+                <BirdBoxModal onToggleBirdBoxModal={handleToggleBirdBoxModal} />
+            </Activity>
+            <Activity mode={isUploadingData ? "visible" : "hidden"}>
+                <UploadModal onToggleUploadModal={handleToggleUploadModal} />
+            </Activity>
+            <div className="div-main-edge-container">
+                <div className="div-dashboard-view-container">
+                    <header className="header-dashboard-view-container">
+                        <div className="div-header-dashboard-view-birdbox-info-container">
+                            <h2>BirdBox 1</h2>
+                            <p>Cornwall Preserve</p>
+                        </div>
+                        <div className="div-dashboard-view-birdbox-editable-container">
+                            <div className="div-birdbox-select-container">
+                                <div className="div-selected-birdbox-info-container" onClick={handleSelectBirdBox}>
+                                    <div className="div-selected-birdbox-name-container">
+                                        <ion-icon src="/media/icons/icon-dot.svg" />
+                                        <p>Box 1 - Cornwall Preserve</p>
+                                    </div>
+                                    <ion-icon src="/media/icons/icon-chevron-down.svg" />
+                                </div>
+                                {selectBirdBox && (
+                                    <ul className="birdbox-options-list">
+                                        <li className="birdbox-options-list-item active">
+                                            <span>Box 1 - Cornwall Preserve</span>
+                                            <ion-icon src="/media/icons/icon-selected.svg" />
+                                        </li>
+                                        <li className="birdbox-options-list-item">
+                                            <span>Box 2 - Deer Creek Woods East</span>
+                                            <ion-icon src="/media/icons/icon-selected.svg" />
+                                        </li>
+                                        <li className="birdbox-options-list-item">
+                                            <span>Box 3 - Deer Creek Woods West</span>
+                                            <ion-icon src="/media/icons/icon-selected.svg" />
+                                        </li>
+                                        <li className="birdbox-options-list-item">
+                                            <span>Box 4 - Ganargua Creek Meadow Preserve</span>
+                                            <ion-icon src="/media/icons/icon-selected.svg" />
+                                        </li>
+                                        <li className="birdbox-options-list-item">
+                                            <span>Box 5 - Irene Gossin Nature Preserve</span>
+                                            <ion-icon src="/media/icons/icon-selected.svg" />
+                                        </li>
+                                    </ul>
+                                )}
+                            </div>
+                            <button className={`${isAddingBox ? "active" : ""}`} onClick={handleToggleBirdBoxModal}>
+                                <ion-icon src="/media/icons/icon-plus.svg" />
+                            </button>
+                        </div>
+                    </header>
+                    <Activity mode={birdBoxData ? "hidden" : "visible"}>
+                        <UploadSign isUploadingData={isUploadingData} onToggleUploadModal={handleToggleUploadModal} />
+                    </Activity>
+                    <Activity mode={birdBoxData ? "visible" : "hidden"}>
+                        <General
+                            isUploadingData={isUploadingData}
+                            onToggleUploadModal={handleToggleUploadModal}
+                            selectedInnerView={selectedInnerView}
+                            onToggleInnerView={handleToggleInnerView}
+                        />
+                        <Activity mode={selectedInnerView === "overview" ? "visible" : "hidden"}>
+                            <Overview onToggleInnerView={handleToggleInnerView} />
+                        </Activity>
+                        <Activity mode={selectedInnerView === "analytics" ? "visible" : "hidden"}>
+                            <Analytics />
+                        </Activity>
+                        <Activity mode={selectedInnerView === "gallery" ? "visible" : "hidden"}>
+                            <Gallery />
+                        </Activity>
+                        <Activity mode={selectedInnerView === "maintenance" ? "visible" : "hidden"}>
+                            <Maintenance />
+                        </Activity>
+                        <Activity mode={selectedInnerView === "settings" ? "visible" : "hidden"}>
+                            <Settings />
+                        </Activity>
+                    </Activity>
+                </div>
+            </div>
+            <NotificationModal />
+        </>
+    );
 };
 
 export default Dashboard;
