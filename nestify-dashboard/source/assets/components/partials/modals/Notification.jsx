8394ec5cca19ea690fcac98944f7180a81d5a390
Balša Bazović <balsa.bazovic@gmail.com>
2026-01-29 13:19:08 -0500

    Updated Notification modal mobile responsiveness.

--- nestify-dashboard/source/assets/components/partials/modals/Notification.jsx
+++ nestify-dashboard/source/assets/components/partials/modals/Notification.jsx
@@ -1,11 +1,14 @@
 // IMPORTED STYLESHEETS
 import "../../../css/partials/modals/notification.css";
+import "../../../css/responsive/partials/modals/notification.css";
 
 const Notification = function () {
     return (
         <div className="div-notification-modal-container">
-            <ion-icon src="/media/icons/icon-notification.svg"></ion-icon>
-            <p>Notification message goes here.</p>
+            <div className="div-notification-modal success">
+                <ion-icon src="/media/icons/icon-notification.svg"></ion-icon>
+                <p>Notification message goes here.</p>
+            </div>
         </div>
     );
 };
