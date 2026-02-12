e8322297f0fc391ed4bb668bfb93a7f6f65b064c
Balša Bazović <balsa.bazovic@gmail.com>
2026-02-12 01:23:30 -0500

    Updated Login view.

--- nestify-dashboard/source/assets/components/views/Login.jsx
+++ nestify-dashboard/source/assets/components/views/Login.jsx
@@ -1,27 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 // IMPORTED STYLESHEETS
 import "../../css/views/login.css";
 // IMPORTED MODULES
 import Logo from "../partials/Logo";
 import LoginForm from "../partials/views/auth/LoginForm";
 import Copyright from "../partials/views/auth/Copyright";
+import PageLoader from "../partials/loaders/Page";
 
 const Login = function () {
-    useEffect(function () {
-        document.title = "Nestify | Login";
-    }, []);
+	const [isViewLoading, setIsViewLoading] = useState(true);
 
-    return (
-        <>
-            <div className="div-login-view-container">
-                <div className="div-login-view-modal">
-                    <Logo />
-                    <LoginForm />
-                </div>
-                <Copyright />
-            </div>
-        </>
-    );
+	useEffect(function () {
+		document.title = "Nestify | Login";
+
+		const loadingTimer = setTimeout(() => {
+			setIsViewLoading(false);
+		}, 2000);
+
+		return () => clearTimeout(loadingTimer);
+	}, []);
+
+	return (
+		<>
+			<PageLoader isViewLoading={isViewLoading} />
+			<div className="div-login-view-container">
+				<div className="div-login-view-modal">
+					<Logo />
+					<LoginForm />
+				</div>
+				<Copyright />
+			</div>
+		</>
+	);
 };
 
 export default Login;
