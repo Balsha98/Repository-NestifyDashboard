bc8178de19af39c60798a179b9f7b13d65339036
Balša Bazović <balsa.bazovic@gmail.com>
2026-02-13 07:21:26 -0500

    Added Profile view.

--- nestify-dashboard/source/app.jsx
+++ nestify-dashboard/source/app.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 // IMPORTED MODULES
-import AppView from "./assets/components/views/App";
+import AppView from "./assets/components/App";
 
 const root = createRoot(document.querySelector(".root"));
 
 root.render(
-    <StrictMode>
-        <BrowserRouter>
-            <AppView />
-        </BrowserRouter>
-    </StrictMode>,
+	<StrictMode>
+		<BrowserRouter>
+			<AppView />
+		</BrowserRouter>
+	</StrictMode>,
 );
---
+++ nestify-dashboard/source/assets/components/App.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+// IMPORTED MODULES
+import LoginView from "./views/Login";
+import SignupView from "./views/Signup";
+import DashboardView from "./views/Dashboard";
+import ReportView from "./views/Report";
+import MapView from "./views/Map";
+import ProfileView from "./views/Profile";
+import InvalidView from "./views/Invalid";
+
+const App = function () {
+	return (
+		<Routes>
+			<Route path="/" element={<LoginView />} />
+			<Route path="/login" element={<LoginView />} />
+			<Route path="/signup" element={<SignupView />} />
+			<Route path="/dashboard" element={<DashboardView />} />
+			<Route path="/report" element={<ReportView />} />
+			<Route path="/map" element={<MapView />} />
+			<Route path="/profile" element={<ProfileView />} />
+			<Route path="*" element={<InvalidView />} />
+		</Routes>
+	);
+};
+
+export default App;
