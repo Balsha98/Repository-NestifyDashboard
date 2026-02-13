bc30a6280afe4534b780b575c6a01eaea0471939
Balša Bazović <balsa.bazovic@gmail.com>
2026-02-13 13:15:00 -0500

    Updated Invalid partial view.

--- nestify-dashboard/source/assets/components/views/Invalid.jsx
+++ nestify-dashboard/source/assets/components/views/Invalid.jsx
@@ -2,20 +2,20 @@ import { useEffect } from "react";
 // IMPORTED STYLESHEETS
 import "../../css/views/invalid.css";
 // IMPORTED MODULES
-import Info from "../partials/views/invalid/Info";
+import Information from "../partials/views/invalid/Information";
 
 const Invalid = function () {
-	useEffect(function () {
-		document.title = "Nestify | Invalid Route";
-	}, []);
+    useEffect(function () {
+        document.title = "Nestify | Invalid Route";
+    }, []);
 
-	return (
-		<div className="div-invalid-view-container">
-			<div className="div-invalid-view-modal">
-				<Info />
-			</div>
-		</div>
-	);
+    return (
+        <div className="div-invalid-view-container">
+            <div className="div-invalid-view-modal">
+                <Information />
+            </div>
+        </div>
+    );
 };
 
 export default Invalid;
