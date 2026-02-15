75374acf97c72278c4d730ca265b4453e7bd337a
Balša Bazović <balsa.bazovic@gmail.com>
2026-02-15 12:51:41 -0500

    Updated Notification modal partial files.

--- nestify-dashboard/source/assets/components/partials/modals/Notification.jsx
+++ nestify-dashboard/source/assets/components/partials/modals/Notification.jsx
@@ -3,14 +3,14 @@ import "../../../css/partials/modals/notification.css";
 import "../../../css/responsive/partials/modals/notification.css";
 
 const Notification = function () {
-	return (
-		<div className="div-notification-modal-container active">
-			<div className="div-notification-modal success">
-				<ion-icon src="/media/icons/icon-check.svg" />
-				<p>Notification message goes here.</p>
-			</div>
-		</div>
-	);
+    return (
+        <div className="div-notification-modal-container">
+            <div className="div-notification-modal success">
+                <ion-icon src="/media/icons/icon-check.svg" />
+                <p>Notification message goes here.</p>
+            </div>
+        </div>
+    );
 };
 
 export default Notification;
