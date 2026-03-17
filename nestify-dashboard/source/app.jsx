f52a59763cab15f8d09f0be9be27a019958fb572
Balša Bazović <balsa.bazovic@gmail.com>
2026-03-17 08:03:20 -0400

    Updated the App component.

--- nestify-dashboard/source/app.jsx
+++ nestify-dashboard/source/app.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 // IMPORTED MODULES
 import AppView from "./assets/components/App";
 
@@ -8,8 +7,6 @@ const root = createRoot(document.querySelector(".root"));
 
 root.render(
 	<StrictMode>
-		<BrowserRouter>
-			<AppView />
-		</BrowserRouter>
+		<AppView />
 	</StrictMode>,
 );
--- nestify-dashboard/source/assets/components/App.jsx
+++ nestify-dashboard/source/assets/components/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // IMPORTED MODULES
 import LoginView from "./views/Login";
 import SignupView from "./views/Signup";
@@ -9,18 +9,20 @@ import ProfileView from "./views/Profile";
 import InvalidView from "./views/Invalid";
 
 const App = function () {
-    return (
-        <Routes>
-            <Route path="/" element={<LoginView />} />
-            <Route path="/login" element={<LoginView />} />
-            <Route path="/signup" element={<SignupView />} />
-            <Route path="/dashboard" element={<DashboardView />} />
-            <Route path="/report" element={<ReportView />} />
-            <Route path="/map" element={<MapView />} />
-            <Route path="/profile" element={<ProfileView />} />
-            <Route path="*" element={<InvalidView />} />
-        </Routes>
-    );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<LoginView />} />
+				<Route path="/login" element={<LoginView />} />
+				<Route path="/signup" element={<SignupView />} />
+				<Route path="/dashboard" element={<DashboardView />} />
+				<Route path="/report" element={<ReportView />} />
+				<Route path="/map" element={<MapView />} />
+				<Route path="/profile" element={<ProfileView />} />
+				<Route path="*" element={<InvalidView />} />
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App;
